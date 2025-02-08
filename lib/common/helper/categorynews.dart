6d788f8a3131b8f2e46c104815b1130a28c7251b
Laurensius Yuda Wicaksono Nugroho <62579710+laurensiusyuda@users.noreply.github.com>
2025-02-08 22:46:02 +0700

    feat : membuat helper untuk date time formater dan category  news

---
+++ lib/common/helper/categorynews.dart
@@ -0,0 +1,12 @@
+class CategoryNews {
+  List<String?> categoryNews = [
+    'Business',
+    'Entertainment',
+    'General',
+    'Health',
+    'Science',
+    'Sports',
+    'Technology',
+    'Politics',
+  ];
+}
---
+++ lib/common/helper/datetimeformatter.dart
@@ -0,0 +1,32 @@
+class DateHelper {
+  String getFormattedDay(DateTime date) {
+    List<String> days = [
+      'Senin',
+      'Selasa',
+      'Rabu',
+      'Kamis',
+      'Jumat',
+      'Sabtu',
+      'Minggu'
+    ];
+    return days[date.weekday - 1];
+  }
+
+  String formatDate(DateTime date) {
+    List<String> months = [
+      'Januari',
+      'Februari',
+      'Maret',
+      'April',
+      'Mei',
+      'Juni',
+      'Juli',
+      'Agustus',
+      'September',
+      'Oktober',
+      'November',
+      'Desember'
+    ];
+    return '${date.day} ${months[date.month - 1]} ${date.year}';
+  }
+}
